ac24d8c0d07e2d9a21dd7ab7e0c3c351a009d5ff
zhegebi <2213665870@qq.com>
2025-10-29 20:02:02 +0800

    Add components: NodePyEdge and NodePyConnection

---
+++ client/src/components/NodePyEdge.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { EdgeProps } from '@vue-flow/core'
+import { BezierEdge } from '@vue-flow/core'
+import { dataTypeColor } from '@/types/nodeTypes'
+
+
+const props = defineProps<EdgeProps>()
+console.log('NodePyEdge props:', props)
+
+</script>
+
+
+<template>
+  <BezierEdge
+      :source-x="sourceX"
+      :source-y="sourceY"
+      :target-x="targetX"
+      :target-y="targetY"
+      :source-position="sourcePosition"
+      :target-position="targetPosition"
+      :curvature="0.1"
+      :style="{ stroke: '#1c6bbf', strokeWidth: 3 }"
+  />
+</template>
