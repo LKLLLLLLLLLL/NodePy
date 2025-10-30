e7d0e86f67c19d0dbab13a8f5863775b9a0aa750
zhegebi <2213665870@qq.com>
2025-10-30 18:54:46 +0800

    Fix bug: sync error

--- client/src/utils/network.ts
+++ client/src/utils/network.ts
@@ -2,36 +2,37 @@ import * as service from '@/utils/api/services/DefaultService'
 import { monitorTask } from './task'
 import type { Project } from './api'
 
-interface SyncResponse{
-    project: Project
-    taskMsg?: any[]
-}
 
-export const syncProject = (p: Project) => {
-    return new Promise<SyncResponse>(async (resolve, reject) => {
+export const syncProject = (p: Project, graphStore: any) => {
+    return new Promise<Project>(async (resolve, reject) => {
         try {
             p.updated_at = Date.now()
+            graphStore.is_syncing = true
             const taskResponse = await service.DefaultService.syncProjectApiProjectSyncPost(p)
+            graphStore.is_syncing = false
             if(taskResponse) {
                 const {task_id} = taskResponse
                 if(task_id) {
                     try {
                         const messages = await monitorTask(p, task_id)
-                        resolve({
-                            project: p,
-                            taskMsg: messages
-                        })
+                        console.log('WS:', messages)
+                        resolve(p)
                     }catch(err) {
+                        graphStore.syncing_err_msg = err instanceof Error ? err.message : String(err)
                         reject(err)
                     }
                 }else {
+                    graphStore.syncing_err_msg = 'No task_id returned'
                     reject('No task_id returned')
                 }
             }else {
-                resolve({project: p})
+                resolve(p)
             }
         }catch(err) {
+            graphStore.syncing_err_msg = err instanceof Error ? err.message : String(err)
             reject(err)
+        }finally {
+            graphStore.is_syncing = false
         }
     })
 }
